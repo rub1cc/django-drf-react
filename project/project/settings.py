9f1db3bb457d9fb5d63639129022336415dd6e12
NVB8D <raz.muhammad14@gmail.com>
2018-05-30 15:11:41 +0800

    Updated procfile

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+%4imw51w)7o_^x_-$2lez-*=ne59@(&3e%gx0q8)m*9(jdp1-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
