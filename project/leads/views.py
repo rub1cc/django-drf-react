58245035d484661ce38c45f5d4711293bb49e1eb
NVB8D <raz.muhammad14@gmail.com>
2018-05-30 11:54:53 +0800

    Create lead serializer and lead list create view

--- project/leads/views.py
+++ project/leads/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from leads.models import Lead
+from leads.serializers import LeadSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.object.all()
+    serializer_class = LeadSerializer
