96d515e3ddddd49c0e8b33a494bf94632fb37456
NVB8D <raz.muhammad14@gmail.com>
2018-05-30 13:55:47 +0800

    Fixed bug

--- project/frontend/src/components/App.js
+++ project/frontend/src/components/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DataProvider from './DataProvider';
-import Table from './Table';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import DataProvider from "./DataProvider";
+import Table from "./Table";
 const App = () => (
-    <DataProvider endpoint="api/lead/" render={data => <Table data={data} />} />
+    <DataProvider endpoint="api/lead/"
+        render={data => <Table data={data} />} />
 );
-
-const wrapper = document.getElementById('app');
-
+const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
--- project/frontend/src/components/DataProvider.js
+++ project/frontend/src/components/DataProvider.js
@@ -1,18 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
 class DataProvider extends Component {
-    static PropTypes = {
+    static propTypes = {
         endpoint: PropTypes.string.isRequired,
         render: PropTypes.func.isRequired
     };
-
     state = {
-        data =[],
-        loaded = false,
-        placeholder = "Loading..."
+        data: [],
+        loaded: false,
+        placeholder: "Loading..."
     };
-
     componentDidMount() {
         fetch(this.props.endpoint)
             .then(response => {
@@ -23,9 +20,9 @@ class DataProvider extends Component {
             })
             .then(data => this.setState({ data: data, loaded: true }));
     }
-
     render() {
         const { data, loaded, placeholder } = this.state;
         return loaded ? this.props.render(data) : <p>{placeholder}</p>;
     }
 }
+export default DataProvider;
